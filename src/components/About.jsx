02e7d7fa806bd9c304c17556c33069695d99687c
Ronnie Rios <ronniesmail14@gmail.com>
2023-02-13 10:49:26 -0600

    addded about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='grid md:grid-cols-3 p-4 justify-items-center gap-4'>
+        <div className='md:col-span-3 mb-4'><h1 className='text-4xl'>Title Title Title</h1></div>
+        <div className='my-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam laudantium nemo itaque sapiente blanditiis perspiciatis illum sint aut corporis culpa molestiae incidunt porro veritatis quasi, sequi recusandae quae magnam sunt!</div>
+        <div className='my-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam laudantium nemo itaque sapiente blanditiis perspiciatis illum sint aut corporis culpa molestiae incidunt porro veritatis quasi, sequi recusandae quae magnam sunt!</div>
+        <div className='my-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam laudantium nemo itaque sapiente blanditiis perspiciatis illum sint aut corporis culpa molestiae incidunt porro veritatis quasi, sequi recusandae quae magnam sunt!</div>
+       
+        
+    </section>
   )
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import About from '../components/About'
 import Landing from '../components/Landing'
 
 const HomePage = () => {
   return (
     <main className='max-w-screen-xl mx-auto'>
         <Landing />
+        <About />
+
     </main>
   )
 }
